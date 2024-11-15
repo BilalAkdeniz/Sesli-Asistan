c8d08b7956b21f3fd3a406844c20fd7e60e52284
BilalAkdeniz <bilalakdeniz002@gmail.com>
2024-11-16 00:20:45 +0300

    v1
    ses alındı butonu eklenmedi...

---
+++ SesliAsistan/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SesliAsistan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Mic2 = new System.Windows.Forms.PictureBox();
+            this.Mic1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.konuşToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seçeneklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Komutlartxt = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Mic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mic1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mic2
+            // 
+            this.Mic2.Image = global::SesliAsistan.Properties.Resources.voice_search;
+            this.Mic2.Location = new System.Drawing.Point(12, 31);
+            this.Mic2.Name = "Mic2";
+            this.Mic2.Size = new System.Drawing.Size(366, 374);
+            this.Mic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Mic2.TabIndex = 0;
+            this.Mic2.TabStop = false;
+            this.Mic2.Click += new System.EventHandler(this.Mic2_Click);
+            // 
+            // Mic1
+            // 
+            this.Mic1.Image = global::SesliAsistan.Properties.Resources.microphone;
+            this.Mic1.Location = new System.Drawing.Point(12, 31);
+            this.Mic1.Name = "Mic1";
+            this.Mic1.Size = new System.Drawing.Size(366, 374);
+            this.Mic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Mic1.TabIndex = 0;
+            this.Mic1.TabStop = false;
+            this.Mic1.Click += new System.EventHandler(this.Mic1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.konuşToolStripMenuItem,
+            this.seçeneklerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(393, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // konuşToolStripMenuItem
+            // 
+            this.konuşToolStripMenuItem.Name = "konuşToolStripMenuItem";
+            this.konuşToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.konuşToolStripMenuItem.Text = "Konuş";
+            this.konuşToolStripMenuItem.Click += new System.EventHandler(this.konuşToolStripMenuItem_Click);
+            // 
+            // seçeneklerToolStripMenuItem
+            // 
+            this.seçeneklerToolStripMenuItem.Name = "seçeneklerToolStripMenuItem";
+            this.seçeneklerToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.seçeneklerToolStripMenuItem.Text = "Seçenekler";
+            this.seçeneklerToolStripMenuItem.Click += new System.EventHandler(this.seçeneklerToolStripMenuItem_Click);
+            // 
+            // Komutlartxt
+            // 
+            this.Komutlartxt.AutoSize = true;
+            this.Komutlartxt.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Komutlartxt.Location = new System.Drawing.Point(135, 49);
+            this.Komutlartxt.Name = "Komutlartxt";
+            this.Komutlartxt.Size = new System.Drawing.Size(115, 25);
+            this.Komutlartxt.TabIndex = 2;
+            this.Komutlartxt.Text = "Komutlar";
+            this.Komutlartxt.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(366, 372);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 420);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Komutlartxt);
+            this.Controls.Add(this.Mic1);
+            this.Controls.Add(this.Mic2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sesli Asistan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Mic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mic1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Mic1;
+        private System.Windows.Forms.PictureBox Mic2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem konuşToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seçeneklerToolStripMenuItem;
+        private System.Windows.Forms.Label Komutlartxt;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SesliAsistan/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SesliAsistan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SpeechRecognitionEngine reco = new SpeechRecognitionEngine();
+        SpeechSynthesizer yanit = new SpeechSynthesizer();
+
+        private void Mic1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Mic1.Visible = false;
+                reco.RecognizeAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Mic2_Click(object sender, EventArgs e)
+        {
+            Mic1.Visible = true;
+        }
+
+        void sesimiTanirsa()
+        {
+            // Algılayabildiği komutlar //
+            string[] komutlar = { "kromu aç", "Komutları Göster", "Alışveriş", "not" };
+            Choices secenekler = new Choices(komutlar);
+            Grammar grammer = new Grammar(new GrammarBuilder(secenekler));
+            reco.LoadGrammar(grammer);
+            reco.SetInputToDefaultAudioDevice();
+            reco.SpeechRecognized += sesimiTanidi;
+
+            foreach(var x in komutlar)
+            {
+                listBox1.Items.Add(x);
+            }
+        }
+
+        private void sesimiTanidi(object sender, SpeechRecognizedEventArgs e)
+        {
+            try
+            {
+                switch (e.Result.Text)
+                {
+                    case "not":
+                        yanit.SpeakAsync("ı am opening");
+                        System.Diagnostics.Process.Start("\"C:\\Users\\bilal\\AppData\\Local\\Programs\\Notion\\Notion.exe\"");
+                        break;
+                    case "Alışveriş":
+                        yanit.SpeakAsync("Ok");
+                        System.Diagnostics.Process.Start("\"C:\\Users\\bilal\\OneDrive\\Masaüstü\\New Internet Shortcut.url\"");
+                        break;
+                    case "kromu aç":
+                        yanit.SpeakAsync("ok");
+                        System.Diagnostics.Process.Start("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"");
+                        break;
+                    case "Komutları Göster":
+                        // Komutları Gösterme Bloğu //
+                        yanit.SpeakAsync("ok");
+                        timer1.Start();
+                        listBox1.Visible = true;
+                        break;
+                    default:
+                        MessageBox.Show("Tanımlanmamış bir komut girdiniz.\n Komutları görmek için KOMUTLARI GÖSTER diyebilirsiniz...");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sesimiTanirsa();
+            // Karşılama //
+            yanit.SpeakAsync("Welcome , Tell me what you need");
+        }
+
+        private void seçeneklerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Mic1.Visible = false;
+            Mic2.Visible = false;
+            Komutlartxt.Visible = true;
+        }
+
+        private void konuşToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Mic1.Visible = true;
+            Mic2.Visible = true;
+            Komutlartxt.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Visible = false;
+            yanit.SpeakAsync("closed");
+        }
+    }
+}
